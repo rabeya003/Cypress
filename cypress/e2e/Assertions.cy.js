504960c24ee4cae0d0880280271fd203a8062bec
rabeya003 <rabeya15-13483@diu.edu.bd>
2024-12-08 16:15:33 +0600

    Implicit_Assertions- check the number of anchor tags present on the website.

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -21,8 +21,36 @@ describe("Assertions demo", () => {
       .and("eq", "OrangeHRM")
       .and("contain", "HRM")
       .and("not.contain", "xhjjanka");
+
+    //Elements
+    cy.get(".orangehrm-login-branding > img").should("be.visible").and("exist");
+    cy.xpath("//a").should("have.length", 5); //number of links are here
+    // Login input
+    cy.get("input[placeholder='Username']").type("Rabeya Boshri"); //provide a value into input box
+    cy.get("input[placeholder='Username']").should(
+      "have.value",
+      "Rabeya Boshri"
+    );
   });
 
   //Explicit assertions
-  it("Explicit assertions", () => {});
+  it("Explicit assertions", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get("input[placeholder='Username']").type("Admin");
+    cy.get("input[placeholder='Password']").type("admin123");
+    cy.get("button[type='submit']").click();
+
+    let expName = "XYZ";
+    cy.get(".oxd-userdropdown-name").then((x) => {
+      let actName = x.text();
+      // BDD
+      expect(expName).to.not.equal(actName);
+      expect(actName).to.equal(expName);
+      // TDD
+      assert.notEqual(expName, expName);
+      assert.equal(expName, expName);
+    });
+  });
 });
