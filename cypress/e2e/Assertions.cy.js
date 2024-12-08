01eb7b3f564701646b3692375a4b4edfc7713f0b
rabeya003 <rabeya15-13483@diu.edu.bd>
2024-12-08 13:14:28 +0600

    Implicit_Assertions-Visit the URL

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -1,5 +1,11 @@
+// const cypress = require("cypress");
+// const { describe } = require("mocha");
 describe("Assertions demo", () => {
-  it("Implicit assertions", () => {});
+  it("Implicit assertions", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+  });
 
   //Explicit assertions
   it("Explicit assertions", () => {});
