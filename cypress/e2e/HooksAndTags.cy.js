3b1ad049b0a73e3ee77b938fc088b3e3bb2da64a
rabeya003 <rabeya15-13483@diu.edu.bd>
2024-12-08 12:44:48 +0600

    write the it blocks

--- cypress/e2e/HooksAndTags.cy.js
+++ cypress/e2e/HooksAndTags.cy.js
@@ -3,22 +3,6 @@
 // BeforeEach
 // AfterEach
 describe("MyTestSuite", () => {
-  before("Search", () => {
-    cy.log("**  Launch app   ***");
-  });
-
-  after(() => {
-    cy.log("*** Close App ****");
-  });
-
-  beforeEach(() => {
-    cy.log("**** Login  ****");
-  });
-
-  afterEach(() => {
-    cy.log("****  logOut ****");
-  });
-
   it("Search", () => {
     cy.log("**** advanced searching  ****");
   });
