b9ad3a0e9d9f376955c283003f6dfe820e433553
rabeya003 <rabeya15-13483@diu.edu.bd>
2024-12-08 12:47:09 +0600

    Cypress E2E Web Automation | Hooks & Tags(beforeEach,afterEach,before,after)

--- cypress/e2e/HooksAndTags.cy.js
+++ cypress/e2e/HooksAndTags.cy.js
@@ -3,6 +3,22 @@
 // BeforeEach
 // AfterEach
 describe("MyTestSuite", () => {
+  before("Search", () => {
+    cy.log("**  Launch app   ***");
+  });
+
+  after(() => {
+    cy.log("*** Close App ****");
+  });
+
+  beforeEach(() => {
+    cy.log("**** Login  ****");
+  });
+
+  afterEach(() => {
+    cy.log("****  logOut ****");
+  });
+
   it("Search", () => {
     cy.log("**** advanced searching  ****");
   });
