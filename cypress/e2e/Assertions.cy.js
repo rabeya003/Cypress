713355c5c22fc0e11b1a689a31ecbc86934e7900
rabeya003 <rabeya15-13483@diu.edu.bd>
2024-12-08 14:28:55 +0600

    Implicit_Assertions-for URL validate

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -5,6 +5,32 @@ describe("Assertions demo", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
+
+    //URL
+    cy.url()
+      .should("include", "orangehrmlive.com")
+      .should(
+        "eq",
+        "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+      )
+      .should("not.contain", "xcvcvhdgcbvngehrm");
+
+    // title
+    cy.title()
+      .should("include", "Orange")
+      .and("eq", "OrangeHRM")
+      .and("contain", "HRM")
+      .and("not.contain", "xhjjanka");
+
+    //Elements
+    cy.get(".orangehrm-login-branding > img").should("be.visible").and("exist");
+    cy.xpath("//a").should("have.length", 5); //number of links are here
+    // Login input
+    cy.get("input[placeholder='Username']").type("Rabeya Boshri"); //provide a value into input box
+    cy.get("input[placeholder='Username']").should(
+      "have.value",
+      "Rabeya Boshri"
+    );
   });
 
   //Explicit assertions
