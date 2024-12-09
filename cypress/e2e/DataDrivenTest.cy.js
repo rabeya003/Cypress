575547b4dca0773a87fd6a061d0b939f92786167
rabeya003 <rabeya15-13483@diu.edu.bd>
2024-12-09 16:06:14 +0600

    DataDriven->validate the Username and password

--- cypress/e2e/DataDrivenTest.cy.js
+++ cypress/e2e/DataDrivenTest.cy.js
@@ -1,10 +1,20 @@
 describe("MyTestSuite", () => {
   it("DataDrivenTest", () => {
-    cy.fixture("orangehrm").then(() => {
+    cy.fixture("orangehrm2.json").then((data) => {
       cy.visit(
         "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
       );
-      data.foreach((userdata) => {});
+
+      // Arrow function
+      data.forEach((userdata) => {
+        cy.get("input[placeholder='Username']").type(userdata.username);
+        cy.get("input[placeholder='Password']").type(userdata.password);
+        cy.get("button[type='submit']").click();
+
+        // validation
+        if (userdata.username == "Admin" && userdata.password == "admin123") {
+        }
+      });
     });
   });
 });
