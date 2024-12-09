b041d5658b09463013888196844dedd559e79357
rabeya003 <rabeya15-13483@diu.edu.bd>
2024-12-09 16:53:15 +0600

    DataDriven-Data Driven testing using the fixture concept with some test data

--- cypress/e2e/DataDrivenTest.cy.js
+++ cypress/e2e/DataDrivenTest.cy.js
@@ -4,7 +4,6 @@ describe("MyTestSuite", () => {
       cy.visit(
         "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
       );
-
       // Arrow function
       data.forEach((userdata) => {
         cy.get("input[placeholder='Username']").type(userdata.username);
@@ -13,6 +12,19 @@ describe("MyTestSuite", () => {
 
         // validation
         if (userdata.username == "Admin" && userdata.password == "admin123") {
+          cy.get(":nth-child(2) > .oxd-main-menu-item > .oxd-text").click();
+          cy.get(
+            ".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module"
+          ).should("have.text", userdata.expected);
+
+          // LogOut
+          cy.get(".oxd-userdropdown-tab").click();
+          cy.get(":nth-child(4) > .oxd-userdropdown-link").click();
+        } else {
+          cy.get(".oxd-text.oxd-text--p.oxd-alert-content-text").should(
+            "have.text",
+            userdata.expected
+          );
         }
       });
     });
