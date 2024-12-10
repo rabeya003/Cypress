108e46941695b1050e46b4c9a3dbb1688978286b
rabeya003 <rabeya15-13483@diu.edu.bd>
2024-12-10 14:35:25 +0600

    RadioButton&Checkbox-Checking Check box  Buttons are visible and checked or not

--- cypress/e2e/RadioBtnAndCheckboxes.cy.js
+++ cypress/e2e/RadioBtnAndCheckboxes.cy.js
@@ -4,3 +4,12 @@ it("Checking Radio Buttons", () => {
   cy.get("input[value='Male']").should("be.visible");
   cy.get("input[value='Male']").check().should("be.checked");
 });
+it("Checkmg Radio Buttons", () => {
+  cy.visit("https://demo.automationtesting.in/Register.html");
+  cy.get("#checkbox1").should("be.visible");
+  cy.get("#checkbox1").check().should("be.checked");
+  //Unselecting checkbox
+  cy.get("#checkbox1").uncheck().should("be.checked");
+  //selecting Another checkbox
+  cy.get("#checkbox3").check().should("be.checked");
+});
