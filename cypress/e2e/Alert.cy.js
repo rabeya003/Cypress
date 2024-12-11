7260877d2242030a05910f931fbffa51d00b6d91
rabeya003 <rabeya15-13483@diu.edu.bd>
2024-12-11 11:45:57 +0600

    JavaScript Confirm alert->Visit the herokuapp website and click on the'Click for JS Confirm'button for open the alert on the screen

--- cypress/e2e/Alert.cy.js
+++ cypress/e2e/Alert.cy.js
@@ -11,4 +11,11 @@ describe("Alerts", () => {
     // Alert window automatically closed by cypress
     cy.get("#result").should("have.text", "You successfully clicked an alert");
   });
+
+  //2) Javascript Confirm Alert:It will have some text with 'OK and 'Cancel' buttons
+  //For OK
+  it("Js Confirm alert-OK", () => {
+    cy.visit("https://the-internet.herokuapp.com/javascript_alerts");
+    cy.get("button[onclick='jsConfirm()']").click();
+  });
 });
