b73c2783503fb13ec57957a29ec9094010641c95
rabeya003 <rabeya15-13483@diu.edu.bd>
2024-12-13 11:55:39 +0600

    Dropdown->Select a country by clicking and selecting.

--- cypress/e2e/Dropdown.cy.js
+++ cypress/e2e/Dropdown.cy.js
@@ -5,4 +5,10 @@ describe("handle dropdown", () => {
       .select("Italy")
       .should("have.value", "Italy");
   });
+  it("Dropdown without select", () => {
+    cy.visit("https://www.dummyticket.com/dummy-ticket-for-visa-application/");
+    cy.get("#select2-billing_country-container").click();
+    cy.get(".select2-search__field").type("Italy").type("{enter}");
+    cy.get("#select2-billing_country-container").should("have.text", "Italy");
+  });
 });
